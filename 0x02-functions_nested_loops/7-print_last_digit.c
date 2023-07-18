836afdb7c141316c2eb3cdbaeb58572d64c54cac
ilyass <ilyass.bentalha2@gmail.com>
2023-07-19 00:59:44 +0100

    betty description

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,5 +1,10 @@
 #include "main.h"
-
+/**
+ * print_last_digit - Calculates and returns the last digit of a given number.
+ * @n: The integer number for which the last digit is to be determined.
+ *
+ * Return: The value of the last digit of the given number.
+ */
 int print_last_digit(int n)
 {
 	int i;
