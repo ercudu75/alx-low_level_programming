80a90c9de0e3decc2a2af2edffbb3ec4ddacbe86
ilyass <ilyass.bentalha2@gmail.com>
2023-07-19 14:06:11 +0100

    fix the problem

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,19 +15,19 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			if (n != 98)
+			if (i != 98)
 		{
 			printf("%d, ", i);
 		}
 		else
-			printf("%d", i);
+			printf("%d\n", i);
 		}
 	}
 	else if (n > 98)
 	{
 		for (i = n; i >= 98; i--)
 		{
-			if (n != 98)
+			if (i != 98)
 		{
 			printf("%d, ", i);
 		}
@@ -36,5 +36,5 @@ void print_to_98(int n)
 		}
 	}
 	else
-	printf("%d ", n);
+	printf("%d\n", n);
 }
