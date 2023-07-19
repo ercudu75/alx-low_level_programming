c45c0dc287c5403948e265bd61380e6d06df1e3c
ilyass <ilyass.bentalha2@gmail.com>
2023-07-19 17:01:07 +0100

    prints the sum of all the multiples of two numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Computes and prints the sum of all the multiples of 3 or 5
+ * below 1024 (excluded).
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num = 1024;
+	int i, sum;
+
+	sum = 0;
+
+	for (i = 0; i < num ; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			sum += i;
+		}
+	}
+	printf("sum is %d", sum);
+
+	return (0);
+}
