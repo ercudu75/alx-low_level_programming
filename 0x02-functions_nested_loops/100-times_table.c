b084377d6d4f9222181e933fa5d7fa2f11339f9b
ilyass <ilyass.bentalha2@gmail.com>
2023-07-19 16:15:02 +0100

    problem fixed

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -21,16 +21,22 @@ void print_times_table(int n)
 		{
 			_putchar(',');
 			if (r < 10)
+			{
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(r + '0');
+			}
 			if (r < 100)
+			{
 				_putchar(' ');
 				_putchar(r + '0');
+			}
 			if (r >= 100)
+			{
 				_putchar(((r / 100) % 10) + '0');
 				_putchar(((r / 10) % 10) + '0');
 				_putchar((r % 10) + '0');
+			}
 		}
 		}
 		_putchar('\n');
