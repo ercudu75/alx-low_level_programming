0695b4bdfdef0bdebcc0a50493553c76c6a97d82
ilyass <ilyass.bentalha2@gmail.com>
2023-07-19 13:50:11 +0100

    problem fixed

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,49 +4,25 @@
  * @n: The starting number.
  *
  * Description: This function prints all the natural numbers from the starting
- *              number (n) up to 98. The numbers are separated by a comma and
- *              space, and they are printed in ascending order.
+ *					number (n) up to 98.
  */
 void print_to_98(int n)
 {
-	if (n <= 98)
-	{
-		int i;
+	int i;
 
-		for (i = n; i <= 98; i++)
+	if (n < 98)
+	{
+		for (i = n; i < 98; i++)
 		{
-			if (i != 98)
-			{
-				_putchar(i / 10 + '0');
-				_putchar(i % 10 + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
-			else
-			{
-			_putchar(i / 10 + '0');
-			_putchar(i % 10 + '0');
-			}
+			printf("%d, ", n);
 		}
 	}
-	else
+	else if (n > 98)
 	{
-		int i;
-
-		for (i = n; i >= 98; i--)
+		for (i = n; i > 98; i--)
 		{
-			if (i != 99)
-			{
-				_putchar(i / 100 + '0');
-				_putchar((i / 10) % 10 + '0');
-				_putchar(i % 100 + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
-			else
-			{
-			_putchar(i / 10 + '0');
-			_putchar(i % 10 + '0');
-			}
+			printf("%d, ", n);
 		}
 	}
+	printf("%d\n", n);
+}
