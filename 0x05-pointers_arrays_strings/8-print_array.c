989ea43cd719012c81301f966ef30fa4701a9801
ilyass <ilyass.bentalha2@gmail.com>
2023-07-24 20:07:55 +0100

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,7 +8,13 @@ void print_array(int *a, int n)
 	printf("%d,",a[0]);
 	for (i = 1; i < n; i++)
 	{
+		if (a[i] != a[n-1])
+		{
 			printf(" %d,",a[i]);
+		}
+		else {
+			printf(" %d",a[n-1]);
+		}
 	}
 	printf("\n");
 }
