8d910debe0316f9895d7dd0930677c6b7928cbee
ilyass <ilyass.bentalha2@gmail.com>
2023-07-24 13:13:07 +0100

    descrition betty

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,5 +1,11 @@
 #include "main.h"
-
+/**
+ * swap_int - Entry point
+ * @a: Takes the pointer as a parameter
+ * @b: Takes the pointer as a parameter
+ * Description: swap the value of two integers
+ * Return: Nothing we have function void
+*/
 void swap_int(int *a, int *b)
 {
 	int temp = *a;
