491d822f4fa4af4e6b652009ed837a12a68a7cce
ilyass <ilyass.bentalha2@gmail.com>
2023-07-24 14:39:24 +0100

    task5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+void rev_string(char *s)
+{
+	int length = 0;
+	int i = 1;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	for (i = length - i; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+}
