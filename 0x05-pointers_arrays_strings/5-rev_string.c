e82369fc08310cb9212f9cb1c93dfc943e1b66ee
ilyass <ilyass.bentalha2@gmail.com>
2023-07-24 21:58:29 +0100

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,18 +12,20 @@ void rev_string(char *s)
 	int length = 0;
 	int start = 0;
 	int end;
+	char temp;
 
 	while (s[length] != '\0')
 	{
 		length++;
 	}
+
 	end = length - 1;
+
 	while (start < end)
 	{
-		char temp = s[start];
+		temp = s[start];
 		s[start] = s[end];
 		s[end] = temp;
-
 		start++;
 		end--;
 	}
