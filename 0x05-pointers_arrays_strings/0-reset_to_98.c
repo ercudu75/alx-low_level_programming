e47e2ef9fec9710b8395fb63c408d73e564a9249
ilyass <ilyass.bentalha2@gmail.com>
2023-07-24 13:00:20 +0100

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - Entry point
+ * @n: Takes the pointer as a parameter
+ * Description: updates the value it points
+ * Return: Nothing we have function void
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
