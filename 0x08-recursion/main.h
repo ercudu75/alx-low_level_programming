5c5dc95e21198e249d04d2f4bfc6e4e11be17776
ilyass <ilyass.bentalha2@gmail.com>
2023-08-03 02:21:38 +0100

    add function

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -13,5 +13,6 @@ int is_prime_number(int n);
 int is_prime(int n, int i);
 int is_palindrome(char *s);
 int check_palindrome(char *s, int left, int right);
+int wildcmp(char *s1, char *s2);
 
 #endif
