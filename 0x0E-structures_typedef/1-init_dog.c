c12a4d225cad3e962f65b7c27f1d4914191f2851
ilyass <ilyass.bentalha2@gmail.com>
2023-08-14 21:48:25 +0100

    function that initialize a variable of type struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,29 @@
+#include "dog.h"
+/**
+ * init_dog - Initializes a struct dog
+ * @d: Pointer to a struct dog variable to be initialized
+ * @name: Pointer to the name of the dog
+ * @age: Age of the dog
+ * @owner: Pointer to the name of the dog's owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	_strcpy(d -> name, name);
+	d -> age = age;
+	_strcpy(d -> owner, owner);
+}
+/**
+ * _strcpy - Custom implementation of strcpy to copy strings
+ * @ptr: Pointer to the destination character array
+ * @p: Pointer to the source character array
+ */
+void _strcpy(char *ptr, char *p)
+{
+	while (*p != '\0')
+	{
+		*ptr = *p;
+		ptr++;
+		p++;
+	}
+	*ptr = '\0';
+}
