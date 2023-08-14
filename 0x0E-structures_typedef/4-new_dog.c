162ce12156c0a449c96ec30cdf4f745bcf09b5db
ilyass <ilyass.bentalha2@gmail.com>
2023-08-14 23:26:20 +0100

    function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,46 @@
 #include "dog.h"
 #include<stdlib.h>
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
 
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+
+/**
+ * _strcpy - Custom implementation of strcpy to copy strings
+ * @ptr: Pointer to the destination character array
+ * @p: Pointer to the source character array
+ */
+void _strcpy(char *ptr, char *p)
+{
+	while (*p != '\0')
+	{
+		*ptr = *p;
+		ptr++;
+		p++;
+	}
+	*ptr = '\0';
+}
+/**
+ * new_dog - creates a new dog structure.
+ * @name: dog's name.
+ * @age: dog's age.
+ * @owner: dog's owner.
+ * Return: the new dog structure.
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dogg;
@@ -23,53 +63,18 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	else
 	{
-		new_dogg->name = _strcpy(new_dogg->name, name);
+		_strcpy(new_dogg->name, name);
 	}
 	new_dogg->owner = malloc(sizeof(char) * (len2 + 1));
 	if (new_dogg->owner == NULL)
 	{
-		free(new_dog->name);
+		free(new_dogg->name);
 		free(new_dogg);
 		return (NULL);
 	}
 	else
 	{
-		new_dogg->owner = _strcpy(new_dogg->owner, owner);
+		_strcpy(new_dogg->owner, owner);
 	}
 	return (new_dogg);
-}
-
-
-/**
- * _strcpy - Custom implementation of strcpy to copy strings
- * @ptr: Pointer to the destination character array
- * @p: Pointer to the source character array
- */
-void _strcpy(char *ptr, char *p)
-{
-	while (*p != '\0')
-	{
-		*ptr = *p;
-		ptr++;
-		p++;
-	}
-	*ptr = '\0';
-}
-/**
- * _strlen - returns the length of a string
- * @s: string to evaluate
- *
- * Return: the length of the string
- */
-int _strlen(char *s)
-{
-	int i;
-
-	i = 0;
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-
-	return (i);
 }
