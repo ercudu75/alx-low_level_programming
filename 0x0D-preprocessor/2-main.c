1ac7bf8921483e0bd3e51c582cbd34f3648dbb0d
ilyass <ilyass.bentalha2@gmail.com>
2023-08-14 17:09:12 +0100

    prints the name of the file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Description: we use Predefined Macros named __FILE__
+ * this command helps us to get the file name
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
