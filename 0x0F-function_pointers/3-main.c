32278a46c4b25f8679e680d3286e6cc694ed5eac
ilyass <ilyass.bentalha2@gmail.com>
2023-08-16 22:00:22 +0100

    a program that performs simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,6 +1,5 @@
 #include "3-calc.h"
-#include<stdlib.h>
-#include<stdio.h>
+
 /**
  * main - performs simple math operations.
  * @argc: argument count
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,6 +1,5 @@
 #include "3-calc.h"
-#include<stdio.h>
-#include<stdlib.h>
+
 /**
  * op_add - adds two integers
  * @a: int 1 to add to int 2
