96f3418290dec9bc0bdb8fa752f4b296c15ea1b0
ilyass <ilyass.bentalha2@gmail.com>
2023-08-25 03:29:04 +0100

    function that prints sentence before the main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,11 @@
+#include <stdlib.h>
 #include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * first - prints before main
  */
-int main(void)
+void __attribute__ ((constructor)) first(void)
 {
-    printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
-    return (0);
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
