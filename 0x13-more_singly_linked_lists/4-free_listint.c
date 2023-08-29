90fc9d40702fb2ca04cd44e007a5bfbbd230ce33
ilyass <ilyass.bentalha2@gmail.com>
2023-08-29 21:24:32 +0100

    function that frees a list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,16 +1,19 @@
 #include "lists.h"
 
 /**
- * free_list - Frees a linked list.
+ * free_listint - Frees a linked list.
  * @head: Pointer to the head of the linked list.
  *
  * This function deallocates memory for all nodes in the linked list.
  */
 void free_listint(listint_t *head)
 {
-	while (head)
+	listint_t *curr;
+
+	while (head != NULL)
 	{
-		free(head);
+		curr = head;
 		head = head->next;
+		free(curr);
 	}
 }
