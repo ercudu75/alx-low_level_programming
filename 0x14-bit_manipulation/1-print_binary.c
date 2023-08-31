b6c3d7bb94a320583aacf5e47eefe73ce71ae65f
ilyass <ilyass.bentalha2@gmail.com>
2023-08-31 23:52:02 +0100

    prints the binary representation of a number

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,2 +1,17 @@
 #include "main.h"
 
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+		_putchar('0');
+	else
+		print_binary_recursive(n);
+
+}
+
+void print_binary_recursive(unsigned long int n)
+{
+	if (n > 1)
+		print_binary_recursive(n >> 1);
+	_putchar((n & 1) + '0');
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,8 +3,10 @@
 #include<stdlib.h>
 #include<stdio.h>
 
+
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
+void print_binary_recursive(unsigned long int n);
 int _putchar(char c);
 
 
