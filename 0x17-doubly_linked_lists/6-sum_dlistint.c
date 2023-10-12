16a20e2086807306d404a0c56ba98035b1e2e208
ilyass <ilyass.bentalha2@gmail.com>
2023-10-13 00:00:27 +0100

    add task 6

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,1 +1,17 @@
-#include "main.h"
+#include "lists.h"
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	if (!head)
+		return 0;
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return sum;
+}
