629fa905aebf45b20b29fdc70ffebed9049b3976
ercudu <ilyass.bentalha2@gmail.com>
2023-11-03 22:29:24 +0000

    add task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,56 @@
+#include "hash_tables.h"
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new_node, *current_node;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (0);
+
+	index = hash_djb2((const unsigned char *)key) % ht->size;
+
+	current_node = ht->array[index];
+
+	while (current_node != NULL)
+	{
+		if (strcmp(current_node->key, key) == 0)
+		{
+			free(current_node->value);
+			current_node->value = strdup(value);
+			if (current_node->value == NULL)
+				return (0);
+			return (1);
+		}
+		current_node = current_node->next;
+	}
+
+	new_node = ht_pair(key, value);
+	if (new_node == NULL)
+		return (0);
+
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
+}
+
+hash_node_t *ht_pair(const char *key, const char *value)
+{
+	hash_node_t *entry = malloc(sizeof(hash_node_t));
+
+	if (entry == NULL)
+		return (NULL);
+
+	entry->key = strdup(key);
+	entry->value = strdup(value);
+	if (entry->key == NULL || entry->value == NULL)
+	{
+		free(entry->key);
+		free(entry->value);
+		free(entry);
+		return (NULL);
+	}
+
+	entry->next = NULL;
+	return (entry);
+}
