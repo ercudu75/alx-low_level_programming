e4379723a13947bb194c69d95a066066d71f2743
ercudu <ilyass.bentalha2@gmail.com>
2023-11-04 00:20:36 +0000

    update task 6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,6 +1,9 @@
 #include "hash_tables.h"
 
-
+/**
+ * hash_table_delete - deletes a hash table.
+ * @ht: pointer to hash table.
+ */
 void hash_table_delete(hash_table_t *ht)
 {
 	hash_node_t *temp_node, *aux;
